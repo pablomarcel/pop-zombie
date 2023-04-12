135d6d559b3c827595ef0677a4fd32dc0f00bd1a
pablomarcel <pablomarcel@outlook.com>
2023-04-11 19:32:38 -0700

    reverted back to 12 column, added a div for no reason

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -87,138 +87,143 @@ const Home = () => {
           <meta property="og:url" content="https://www.pop-zombie.com" />
           <meta property="og:description" content="Explore the world of Pop Zombie, your go-to hub for captivating content on travel, lifestyle, and more. Join us, share your favorites, and discover your next adventure! " />
         </Helmet>
-        <Container fluid>
-          <Row>
 
-            <Col>
-              {/* Rendering button showing/hiding slide show */}
-              <div className="right__side mt-2">
-                <Button
-                    variant="primary"
-                    size="sm"
-                    onClick={() => setToggleSlideShow(!toggleSlideShow)}
-                    style={{
-                      backgroundColor: '#c3edbf',
-                      borderColor: '#c3edbf',
-                      width:'13%'
-                    }}
-                >
-                  {toggleSlideShow ? "Hide Slide Show" : "Show Slide Show"}
-                </Button>
-              </div>
-            </Col>
-          </Row>
+        <div>
+          <Container fluid>
+            <Row>
 
-          <Row>
-            {/* Dividing the screen into 3 columns */}
-            <Col xs={8} md={4}>
-              <Row className="mt-2" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <Col>
-                  <h3 style={{
-                    color: '#80E1D1',
-                    fontSize:'22px',
-                  }}>Categories</h3>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <div className="d-grid gap-2">
-                    {categories.map((category, index) => (
-                        <Button
-                            key={index}
-                            variant="outline-primary"
-                            onClick={() => handleCategoryClick(category)}
-                            style={{
-                              backgroundColor: '#FFA07A',
-                              borderColor: '#FFA07A',
-                              width:'50%'
-                            }}
-                        >
-                          {category}
-                        </Button>
-                    ))}
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-
-            <Col xs={8} md={4} className="d-flex justify-content-center align-items-center">
-
-              {/* Post-List area */}
               <Col>
-                <div>
-                  <Row className="mb-2">
-                    <Col>
-                      <Form className='mt-2'>
-                        <FormControl
-                            type="search"
-                            placeholder="Search..."
-                            className="me-5"
-                            aria-label="Search"
-                            onChange={searchPost} // onChange will trigger "search post"
-
-                        />
-                      </Form>
-                    </Col>
-
-                  </Row>
+                {/* Rendering button showing/hiding slide show */}
+                <div className="right__side mt-2">
+                  <Button
+                      variant="primary"
+                      size="sm"
+                      onClick={() => setToggleSlideShow(!toggleSlideShow)}
+                      style={{
+                        backgroundColor: '#c3edbf',
+                        borderColor: '#c3edbf',
+                        width:'13%'
+                      }}
+                  >
+                    {toggleSlideShow ? "Hide Slide Show" : "Show Slide Show"}
+                  </Button>
                 </div>
+              </Col>
+            </Row>
 
+            <Row>
+              {/* Dividing the screen into 3 columns */}
+              <Col xs={12} md={4}>
+                <Row className="mt-2" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <Col>
+                    <h3 style={{
+                      color: '#80E1D1',
+                      fontSize:'22px',
+                    }}>Categories</h3>
+                  </Col>
+                </Row>
                 <Row>
-                  {postList.length > 0 ? (
-                      <ListGroup variant="flush" as="ol">
-                        {
-                          postList.map((post) => {
-                            return (
-                                <ListGroup.Item key={post._id}>
-                                  <div className="fw-bold h4" style={{ color: "#fbb9c5" }}>
-                                    <Link to={`/postDetail/${post._id}`} style={{ textDecoration: 'none', color: "#f17891" }}>{post.title}</Link>
-                                  </div>
-                                  <div style={{ color: "#b8dfe6" }}>{post.content} - <span className="text-secondary">{`Last modified: ${formatDistance(new Date(post.updatedAt), new Date())}`}</span></div>
-                                </ListGroup.Item>
-                            );
-                          })
-                        }
-                      </ListGroup>) : null}
+                  <Col>
+                    <div className="d-grid gap-2">
+                      {categories.map((category, index) => (
+                          <Button
+                              key={index}
+                              variant="outline-primary"
+                              onClick={() => handleCategoryClick(category)}
+                              style={{
+                                backgroundColor: '#FFA07A',
+                                borderColor: '#FFA07A',
+                                width:'50%'
+                              }}
+                          >
+                            {category}
+                          </Button>
+                      ))}
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+
+              <Col xs={12} md={4} className="d-flex justify-content-center align-items-center">
+
+                {/* Post-List area */}
+                <Col>
                   <div>
                     <Row className="mb-2">
-                      {/* Image Slide Show area */}
-                      <div className={toggleSlideShow ? "show" : "hide"}>
-                        <h3 className='title mb-2' style={{ color: "#b99ef5" }}>Pop Zombie Slide Show</h3>
-                        {posts.length > 0 ? (<ImageSlideShow posts={posts} />) : ('')}
-                      </div>
+                      <Col>
+                        <Form className='mt-2'>
+                          <FormControl
+                              type="search"
+                              placeholder="Search..."
+                              className="me-5"
+                              aria-label="Search"
+                              onChange={searchPost} // onChange will trigger "search post"
+
+                          />
+                        </Form>
+                      </Col>
+
                     </Row>
                   </div>
-                  <h3 className='title mb-2' style={{ color: "#f17891" }}>Pop Zombie List</h3>
-                  {posts.length > 0 ? (
-                      <>
-                        {displayPosts}
-                      </>
-                  ) : ('')
-                  }
-                </Row>
+
+                  <Row>
+                    {postList.length > 0 ? (
+                        <ListGroup variant="flush" as="ol">
+                          {
+                            postList.map((post) => {
+                              return (
+                                  <ListGroup.Item key={post._id}>
+                                    <div className="fw-bold h4" style={{ color: "#fbb9c5" }}>
+                                      <Link to={`/postDetail/${post._id}`} style={{ textDecoration: 'none', color: "#f17891" }}>{post.title}</Link>
+                                    </div>
+                                    <div style={{ color: "#b8dfe6" }}>{post.content} - <span className="text-secondary">{`Last modified: ${formatDistance(new Date(post.updatedAt), new Date())}`}</span></div>
+                                  </ListGroup.Item>
+                              );
+                            })
+                          }
+                        </ListGroup>) : null}
+                    <div>
+                      <Row className="mb-2">
+                        {/* Image Slide Show area */}
+                        <div className={toggleSlideShow ? "show" : "hide"}>
+                          <h3 className='title mb-2' style={{ color: "#b99ef5" }}>Pop Zombie Slide Show</h3>
+                          {posts.length > 0 ? (<ImageSlideShow posts={posts} />) : ('')}
+                        </div>
+                      </Row>
+                    </div>
+                    <h3 className='title mb-2' style={{ color: "#f17891" }}>Pop Zombie List</h3>
+                    {posts.length > 0 ? (
+                        <>
+                          {displayPosts}
+                        </>
+                    ) : ('')
+                    }
+                  </Row>
 
 
-                {/* Rendering the pagination buttons */}
-                <Row className='pt-2'>
-                  <ReactPaginate
-                      previousLabel={<FcPrevious />}
-                      nextLabel={<FcNext />}
-                      pageCount={pageCount}
-                      onPageChange={changePage}
-                      containerClassName={"paginationBttns"}
-                      previousLinkClassName={"previousBttn"}
-                      nextLinkClassName={"nextBttn"}
-                      disabledClassName={"paginationDisabled"}
-                      activeClassName={"paginationActive"}
-                  />
-                </Row>
+                  {/* Rendering the pagination buttons */}
+                  <Row className='pt-2'>
+                    <ReactPaginate
+                        previousLabel={<FcPrevious />}
+                        nextLabel={<FcNext />}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        containerClassName={"paginationBttns"}
+                        previousLinkClassName={"previousBttn"}
+                        nextLinkClassName={"nextBttn"}
+                        disabledClassName={"paginationDisabled"}
+                        activeClassName={"paginationActive"}
+                    />
+                  </Row>
 
+                </Col>
               </Col>
-            </Col>
+            </Row>
+          </Container>
 
-          </Row>
-        </Container>
+        </div>
+
+
       </>
   );
 
